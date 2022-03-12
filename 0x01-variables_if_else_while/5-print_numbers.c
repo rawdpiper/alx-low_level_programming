b30c1a98d28be545c8bf56af01bc7ef5653a8582
Hamzat Azeez <azeezhamzy99@gmail.com>
2022-03-12 10:40:28 +0100

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,15 +2,11 @@
 
 int main(void)
 {
-
-	int i;
-
-    
-	for (i = 0; i <= 9; i++)
-	{
-		printf("%d", i);
-	}
-	printf("\n");
-
-    return (0);
+int i;    
+for (i = 0; i <= 9; i++)
+{
+printf("%d", i);
+}
+printf("\n");
+return (0);
 }
