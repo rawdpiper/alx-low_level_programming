eeb989496fda680a139929556f725de353e0877a
Hamzat Azeez <azeezhamzy99@gmail.com>
2022-03-12 12:15:27 +0100

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,11 +9,11 @@ int main()
 {
 int i, j, k;
 
-for (int i = 0; i <= 9; i++)
+for (i = 0; i <= 9; i++)
 {
-for (int j = i + 1; j <= 9; j++)
+for (j = i + 1; j <= 9; j++)
 {
-for (int k = j + 1; k <= 9; k++)
+for (k = j + 1; k <= 9; k++)
 {
 putchar(i + '0');
 putchar(j + '0');
