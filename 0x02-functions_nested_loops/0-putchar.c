e285e58db1ef6f4d0b1e11cc724b79ab7d2470e6
Hamzat Azeez <azeezhamzy99@gmail.com>
2022-03-16 09:07:40 +0100

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ * 
+ * Return: 0 on success. Error code otherwise
+ */
+
+void print_puchar(void)
+{
+	_putchar("_putchar")
+	return (0)
+}
