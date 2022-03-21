90ab2d43261f816affdc8a7ae576b50317d9347b
Hamzat Azeez <azeezhamzy99@gmail.com>
2022-03-21 12:37:27 +0100

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,23 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * puts2 - prints one char out of 2
- * @str: type
- *
- */
+ * puts2 - prints a string, followed by a new line,
+ * @str: pointer to the string to print
+ * Return: void
+*/
 
 void puts2(char *str)
 {
-	int i, j = 0;
 
-	while (str[i] != '\0')
+int i = 0;
+
+while (str[i] != '\0')
+{
+	if (i % 2 == 0)
 	{
-		i++;
+		_putchar(str[i]);
 	}
 
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
-	}
-	_putchar('\n');
+	i++;
+}
+_putchar('\n');
 }
