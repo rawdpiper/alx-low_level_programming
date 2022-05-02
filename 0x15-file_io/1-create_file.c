5a119169b9f9eb4431bbc6cd1ed3b95dbcc43ed0
Hamzat Azeez <azeezhamzy99@gmail.com>
2022-05-02 21:44:50 +0100

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,32 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * create_file - creates a file and fills it with text
- * @filename: name of the file to create
- * @text_content: text to write in the file
- *
+ * create_file - create a file with read/write access for user
+ * @filename: name of file to create
+ * @text_content: string to write to file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, x, y = 0;
+int fd, rstatus, i;
 
-	if (!filename)
+if (filename == NULL)
+	return (-1);
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+if (fd == -1)
+	return (-1);
+
+if (text_content)
+{
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	rstatus = write(fd, text_content, i);
+	if (rstatus == -1)
 		return (-1);
+}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (fd < 0)
-		return (-1);
-
-	if (text_content)
-	{
-		while (text_content[y])
-			y++;
-		x = write(fd, text_content, y);
-		if (x != y)
-			return (-1);
-	}
-
-	close(fd);
-
-	return (1);
+close(fd);
+return (1);
 }
